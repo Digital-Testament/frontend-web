a231c659171c99b2500092d9e7080c394c59549c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-12 20:20:33 -0300

    chore: import tailwindcss on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
+ import "tailwindcss/tailwind.css"
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
       <Component {...pageProps} />
