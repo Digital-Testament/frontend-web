b7a65ba723e17e1158f099c1a238e8d4852a4d82
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-02-12 18:46:13 -0300

    revert: remove styled-components from _app Remove components and imports from styled-components Import global css file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
 
-import GlobalStyle from '../styles/global'
-import defaultTheme from '../styles/theme'
+import '../styles/global.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
   )
 }
 
